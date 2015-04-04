5fe4564366f69641e1ebd3e35b31691edd1d015d
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-04-04 11:32:07 +0530

    Initial Commitment

---
+++ EFConnectionResiliency/EFConnectionResiliency/EmployeeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+
+namespace EFConnectionResiliency
+{
+    public class EmployeeConfiguration : DbConfiguration
+    {
+        public EmployeeConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+        }
+    }
+}
---
+++ EFConnectionResiliency/EFConnectionResiliency/EmployeeContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace EFConnectionResiliency
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(): base("DefaultConnectionString")
+        {
+            // public constructor to use connection string
+        }
+        public DbSet<Employee> Employees { get; set; } 
+    }
+}
